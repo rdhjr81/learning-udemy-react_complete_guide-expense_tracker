7c10a938f8528fda8643a1ee01d662e1c98ad309
rob <rdhjr81@gmail.com>
2022-08-16 14:32:23 -0400

    add expense date component

--- src/App.js
+++ src/App.js
@@ -30,17 +30,22 @@ function App() {
         title={expenses[0].title}
         date={expenses[0].date}
         amount={expenses[0].amount}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         date={expenses[1].date}
         amount={expenses[1].amount}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         date={expenses[2].date}
         amount={expenses[2].amount}
-      ></ExpenseItem>
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        date={expenses[3].date}
+        amount={expenses[3].amount}
+      />
     </div>
   );
 }
