c2e00aef43d4e7ed7d3f00db100b2505092e6531
rob <rdhjr81@gmail.com>
2022-08-17 16:06:55 -0400

    add expense list component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
+
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
@@ -21,14 +22,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            amount={expense.amount}
-          />
-        ))}
+        <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
   );
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,22 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+const ExpensesList = (props) => {
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (props.expenses.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
+  }
+  return (
+    <ul className="expenses-list">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          date={expense.date}
+          title={expense.title}
+          amount={expense.amount}
+        />
+      ))}
+    </ul>
+  );
+};
+export default ExpensesList;
