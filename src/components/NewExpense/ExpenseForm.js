8871c0d959e4442f215ca9947264f0ffabe05418
rob <rdhjr81@gmail.com>
2022-08-18 15:04:56 -0400

    fix input bug

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,10 +23,11 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
+    console.log(expenseData);
     props.onSaveExpenseData(expenseData);
 
     setEnteredTitle("");
